a8bb468ca388836609078d1f75f1df0118343d99
irsanrochendi <irsanrochendi@users.noreply.github.com>
2026-07-13 17:00:39 +0700

    dev: change frontend dev port to 5173

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
-    port: 80,
+    port: 5173,
     allowedHosts: ['.ast.local', 'access.ast.local'],
     proxy: {
       '/api': {
